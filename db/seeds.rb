eefc4fc6c73fde8b1569c1072ca7c7a4b76b72bb
vagrant <vagrant@localhost.localdomain>
2020-09-16 08:28:07 +0000

    seeds.rb変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-	name: 'test',
-	email: 'test@test',
-	password: '000000',
-	password_confirmation: '000000'
-	)
