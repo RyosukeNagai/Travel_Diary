51d58535517eee59f6c5cd4d498fe1401e2de707
vagrant <vagrant@localhost.localdomain>
2021-04-09 16:49:53 +0000

    レイアウト変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,27 +17,44 @@
 //= require bootstrap-sprockets
 //= require_tree .
 // オプションを指定してSkipprの実行
-$(document).on('turbolinks:load', function(){
-	$(".theTarget").skippr({
-	  // スライドショーの変化（"fade" or "slide"）
-	  transition : 'fade',
-	  // 変化にかかる時間（ミリ秒）
-	  speed : 2000,
-	  // easingの種類
-	  easing : 'easeOutQuart',
-	  // ナビゲーションの形（"block" or "bubble"）
-	  navType : 'block',
-	  // 子要素の種類（"div" or "img"）
-	  childrenElementType : 'div',
-	  // ナビゲーション矢印の表示（trueで表示）
-	  arrows : false,
-	  // スライドショーの自動再生（falseで自動再生なし）
-	  autoPlay : true,
-	  // 自動再生時のスライド切替間隔（ミリ秒）
-	  autoPlayDuration : 3000,
-	  // キーボードの矢印キーによるスライド送りの設定（trueで有効）
-	  keyboardOnAlways : true,
-	  // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-	  hidePrevious : false
+
+// //ハンバーガーメニュー
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle(1000);
+    event.preventDefault();
+  });
+});
+
+$(function() {
+  // 一旦hide()で隠してフェードインさせる
+  $('.main-visual__text').hide().fadeIn(3000);
+  $('.main-visual__texts').hide().fadeIn(5000);
+});
+//テキストのフェードイン
+$(function(){
+	$(window).scroll(function (){
+			$('.main-visual__sub-text, .down-to-top').each(function(){
+					var elemPos = $(this).offset().top;
+					var scroll = $(window).scrollTop();
+					var windowHeight = $(window).height();
+					if (scroll > elemPos - windowHeight + 150){
+							$(this).addClass('scrollin');
+					}
+			});
+	});
+});
+
+$(function(){
+	$(window).scroll(function (){
+			$('.fadein').each(function(){
+					var position = $(this).offset().top;
+					var scroll = $(window).scrollTop();
+					var windowHeight = $(window).height();
+					if (scroll > position - windowHeight + 2600){
+						$(this).addClass('active');
+					}
+			});
 	});
 });
