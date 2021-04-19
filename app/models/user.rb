96986c354020c0a44b2111116356a288b6f1730b
vagrant <vagrant@localhost.localdomain>
2021-04-19 12:51:56 +0000

    login/sign-upレイアウト変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,13 @@ class User < ApplicationRecord
 
   attachment :user_image,destroy: false
 
-  validates :name, length: {minimum: 2, maximum: 20}
   validates :introduce,length: { maximum: 30}
-
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 }
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+    validates :password, presence: true,
+              format: { with: VALID_PASSWORD_REGEX,
+               message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
 end
