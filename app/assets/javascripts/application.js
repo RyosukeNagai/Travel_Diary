7ad4a5d534b00e6bdeeda25433f01224c7eb5f6c
vagrant <vagrant@localhost.localdomain>
2021-04-26 07:03:16 +0000

    Homeレイアウト変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,33 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+	//top下slider
 // オプションを指定してSkipprの実行
+$(document).ready(function(){
+	$("#skippr-slider").skippr({
+		transition : 'fade',
+	// 変化にかかる時間（ミリ秒）
+	speed : 4000,
+	// easingの種類
+	easing : 'easeOutQuart',
+	// ナビゲーションの形（"block" or "bubble"）
+	navType : 'block',
+	// 子要素の種類（"div" or "img"）
+	childrenElementType : 'div',
+	// ナビゲーション矢印の表示（trueで表示）
+	arrows : false,
+	// スライドショーの自動再生（falseで自動再生なし）
+	autoPlay : true,
+	// 自動再生時のスライド切替間隔（ミリ秒）
+	autoPlayDuration : 5000,
+	// キーボードの矢印キーによるスライド送りの設定（trueで有効）
+	keyboardOnAlways : true,
+	// 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
+	hidePrevious : false
+	});
+});
+
 
 // //ハンバーガーメニュー
 $(function() {
@@ -109,4 +135,3 @@ $(window).scroll(function(){
 			$('.service-lefts').removeClass('start');
 		}
 	});
-
